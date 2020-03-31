7603d0ad1e2efb579292e6a922b083c8c0b7211d
JonathanYde <jonathan.yde@biomed.au.dk>
2020-03-31 12:33:29 +0200

    finishes a bug, where the 30-day death rates were evaluated as strings and not as numbers

--- Week4_assignment.R
+++ Week4_assignment.R
@@ -18,13 +18,18 @@ hist(outcome[,11], main = "30-day death rates", xlab = "30-day death rate") #pri
 #I removed outcome as an argument - instead the function prompts you for either heart attack, heart failure or pneumonia
 #I added an optional argument (with "outcome-of-care-measures.csv" as default value), so I may change the .csv-file to be read
 best <- function(state, data = "outcome-of-care-measures.csv"){
-        df <- read.csv(data, colClasses = "character")
-        if(!state %in% df$State){print("invalid state")}
-        print("worked so far")
-        subs <- subset(df, df$State == state)
+        df <- read.csv(data,  colClasses = "character") #reads the .csv-file
+        suppressWarnings({ #removes the warnings produced by coercing strings into numbers
+        df[,11] <- as.numeric(df[,11]) #changes heart attacks into numbers
+        df[,17] <- as.numeric(df[,17]) #changes heart failures into numbers
+        df[,23] <- as.numeric(df[,23]) #changes pneumonias into numbers
+        })
+        if(!state %in% df$State){print("invalid state")} else {#tests whether the given state is in the dataset
+        subs <- subset(df, df$State == state) #subsets the dataframe by the given state
+        #the next line prompts the user to select the outcome measure from a list
         oc <- select.list(c("Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure", "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"), graphics = T, title = "Choose between heart attack, heart failure or pneumonia")
-        low <- subset(subs, subs[[oc]]==min(na.omit(subs[[oc]])))[, "Hospital.Name"]
-        min(low)
+        low <- subset(subs, subs[[oc]]==min(na.omit(subs[[oc]])))[, "Hospital.Name"] #selects the hospitals with the lowest mortality
+        min(low) #selects the hospital with the lowest mortality that is first alphabetically
+        }
 }
-best("NY")
-
+best(state = "MD")
